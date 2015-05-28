55a84cbc9b75ce807ce0c58ba6f1d16660306e1c
Horst Schneider <h.schneider@hs-mannheim.de>
2015-05-28 08:42:03 +0200

    added pdf and web build to build script

--- build.ps1
+++ build.ps1
@@ -1,17 +1,28 @@
 Function clean
 {
-	gci -Recurse *.aux | rm 
-	gci -Recurse *.log | rm 
-	gci -Recurse *.dvi | rm 
-	gci -Recurse *.synctex.gz| rm 
-	gci -Recurse *.pdf | rm 
+	delete-temps
+	gci -Recurse *.pdf, *.css, *.html, *.htm | rm 
 	
+}
+
+Function delete-temps
+{
+	gci -Recurse *.aux, *.log, *.dvi, *.synctex.gz, *.4ct, *.4tc, *.tmp, *.lg, *.idv, *.xref | rm 
 }
 
 Function build
 {
 	cd patterns
-	gci -Exclude template.tex *.tex | ForEach-Object {pdflatex -output-directory "../output" $_.FullName }
+	gci -Exclude template.tex *.tex | ForEach-Object {pdflatex -output-directory "../output/pdf" $_.FullName }
+	gci ../output/pdf/ -Exclude *.pdf | ForEach-Object {mv $_ ../output/temp/pdf}
 	cd ..
 }
 
+Function build-web
+{
+	cd patterns
+	gci -Exclude template.tex *.tex | ForEach-Object {htlatex $_.FullName "html, -css, charset=utf-8" " -cunihtf -utf8"}
+	gci -Exclude *.html, *.tex | ForEach-Object {mv $_ ../output/temp/html}
+	mv *.html ../output/html
+	cd ..
+}
