01d2a550c721818d9251e53bd1207ad3f103ae59
Horst Schneider <h.schneider@hs-mannheim.de>
2015-06-24 16:03:39 +0200

    fixed png issue

--- build.ps1
+++ build.ps1
@@ -15,7 +15,7 @@ Function build
 	New-Item -Force -ItemType directory -Path output/pdf
 	New-Item -Force -ItemType directory -Path output/temp/pdf
 	cd patterns
-	gci -Exclude template.tex *.tex | ForEach-Object {pdflatex -output-directory "../output/pdf" $_.FullName }
+	gci -Exclude template.tex *.tex | ForEach-Object {pdflatex -interaction=nonstopmode -output-directory "../output/pdf" $_.FullName }
 	gci ../output/pdf/ -Exclude *.pdf | ForEach-Object {mv -Force $_ ../output/temp/pdf}
 	cd ..
 }
@@ -26,9 +26,9 @@ Function build-web
 	New-Item -Force -ItemType directory -Path output/temp/html
 	cd patterns
 	gci -Exclude template.tex *.tex | ForEach-Object {htlatex $_.FullName "html, -css, charset=utf-8" " -cunihtf -utf8"}
-	gci -Exclude *.html, *.tex | ForEach-Object {mv -Force $_ ../output/temp/html}
+	gci -Exclude *.html, *.tex, *.png | ForEach-Object {mv -Force $_ ../output/temp/html}
 	mv -Force *.html ../output/html
-	
+	cp *.png ../output/html
 
 	cd ../output/html
 	New-Item -ItemType file -Force index.html
